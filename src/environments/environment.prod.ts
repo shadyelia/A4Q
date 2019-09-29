dd44e5c4886ed04297279315308ef449822b47cb
ShadyElia <shady.elia@hotmail.com>
2019-09-29 21:48:11 +0200

    add env to heroku

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB01WLgtZBJlMDeu4612Wro2DXDEHvceUY",
+    authDomain: "a4qs-a7393.firebaseapp.com",
+    databaseURL: "https://a4qs-a7393.firebaseio.com",
+    projectId: "a4qs-a7393",
+    storageBucket: "a4qs-a7393.appspot.com",
+    messagingSenderId: "706590034386",
+    appId: "1:706590034386:web:4e049525baa957c28f200b",
+    measurementId: "G-ZJMF535P8R"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB01WLgtZBJlMDeu4612Wro2DXDEHvceUY",
+    authDomain: "a4qs-a7393.firebaseapp.com",
+    databaseURL: "https://a4qs-a7393.firebaseio.com",
+    projectId: "a4qs-a7393",
+    storageBucket: "a4qs-a7393.appspot.com",
+    messagingSenderId: "706590034386",
+    appId: "1:706590034386:web:4e049525baa957c28f200b",
+    measurementId: "G-ZJMF535P8R"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
