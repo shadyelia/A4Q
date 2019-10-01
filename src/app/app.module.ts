2d0a9387194fdd73b8044ca04e3937b2db4a6448
shadyelia <shady.elia@orca-tech.net>
2019-10-01 11:07:11 +0200

    cleaning up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatCardModule } from "@angular/material/card";
+import { MatSelectModule } from "@angular/material/select";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatButtonModule } from "@angular/material/button";
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
 
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { QuestionsListComponent } from './questions-list/questions-list.component';
-import { QuestionDataComponent } from './question-data/question-data.component';
-import { environment } from '../environments/environment';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { QuestionsListComponent } from "./questions-list/questions-list.component";
+import { QuestionDataComponent } from "./question-data/question-data.component";
+import { environment } from "../environments/environment";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    QuestionsListComponent,
-    QuestionDataComponent
-  ],
+  declarations: [AppComponent, QuestionsListComponent, QuestionDataComponent],
   imports: [
     NgbModule,
     FormsModule,
@@ -32,6 +27,7 @@ import { environment } from '../environments/environment';
     MatCardModule,
     MatSelectModule,
     MatCheckboxModule,
+    MatButtonModule,
 
     BrowserModule,
     AppRoutingModule,
@@ -43,4 +39,4 @@ import { environment } from '../environments/environment';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
